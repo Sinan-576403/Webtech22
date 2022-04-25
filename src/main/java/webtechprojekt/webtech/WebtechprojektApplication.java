47fd54bd60e4bd298f024bb74383793e0a0449db
sinan <s0576403@htw-berlin.de>
2022-04-25 11:23:15 +0200

    init

---
+++ src/main/java/webtechprojekt/webtech/WebtechprojektApplication.java
@@ -0,0 +1,13 @@
+package webtechprojekt.webtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechprojektApplication.class, args);
+	}
+
+}
